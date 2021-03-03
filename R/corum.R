9d21c45074d3054af8f7c6cbc747e4c1738377f0
lgeistlinger <ludwig.geistlinger@gmail.com>
2021-03-02 20:43:44 -0500

    corum: convert to list

--- R/corum.R
+++ R/corum.R
@@ -34,3 +34,27 @@ getCorum <- function(set = c("all", "core", "splice"),
     file.remove(c(set, paste0(set, ".zip")))
     return(corum) 
 }
+
+#' @export
+corum2list <- function(corum.df, 
+                       subunit.id.type = c("UNIPROT", "ENTREZID"),
+                       organism = "Human")
+{
+    subunit.id.type <- match.arg(subunit.id.type)
+    stopifnot(organism %in% corum.df$Organism)
+    
+    corum.df <- subset(corum.df, Organism == organism)
+    subunit.id.type <- ifelse(subunit.id.type == "UNIPROT", "UniProt", "Entrez")
+    id.col <- paste0("subunits(", subunit.id.type, " IDs)")
+    
+    gs <- strsplit(corum.df[[id.col]], " ?; ?")
+    complex.name <- gsub(" ", "_", corum.df$ComplexName)
+    names(gs) <- paste0("CORUM", corum.df$ComplexID, "_", complex.name)
+    return(gs)
+}
+    
+corum2GeneSetCollection <- function(corum.df)
+{
+    
+}
+
